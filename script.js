7fbe5974931fdea39fdee17f02c73e378f2796a6
SkellyBG <skellybg245@gmail.com>
2023-09-05 23:20:27 +1000

    cursor now only show pointer on hobbies that aren't current

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 // document.querySelector('.hobby-puzzling-content').classList.add('.hidden');
 document.querySelector('.hobby-baking-content').classList.add('hidden');
 document.querySelector('.hobby-board-gaming-content').classList.add('hidden');
+document.querySelector('.hobby-baking').classList.add('clickable');
+document.querySelector('.hobby-board-gaming').classList.add('clickable');
 
 document.querySelector('.hobby-puzzling').addEventListener('click', () => {
   document.querySelector('.hobby-puzzling-content').classList.remove('hidden');
   document.querySelector('.hobby-baking-content').classList.add('hidden');
   document.querySelector('.hobby-board-gaming-content').classList.add('hidden');
+  document.querySelector('.hobby-puzzling').classList.remove('clickable');
+  document.querySelector('.hobby-baking').classList.add('clickable');
+  document.querySelector('.hobby-board-gaming').classList.add('clickable');
 });
 
 document.querySelector('.hobby-baking').addEventListener('click', () => {
   document.querySelector('.hobby-puzzling-content').classList.add('hidden');
   document.querySelector('.hobby-baking-content').classList.remove('hidden');
   document.querySelector('.hobby-board-gaming-content').classList.add('hidden');
+  document.querySelector('.hobby-puzzling').classList.add('clickable');
+  document.querySelector('.hobby-baking').classList.remove('clickable');
+  document.querySelector('.hobby-board-gaming').classList.add('clickable');
 });
 
 document.querySelector('.hobby-board-gaming').addEventListener('click', () => {
   document.querySelector('.hobby-puzzling-content').classList.add('hidden');
   document.querySelector('.hobby-baking-content').classList.add('hidden');
   document.querySelector('.hobby-board-gaming-content').classList.remove('hidden');
+  document.querySelector('.hobby-puzzling').classList.add('clickable');
+  document.querySelector('.hobby-baking').classList.add('clickable');
+  document.querySelector('.hobby-board-gaming').classList.remove('clickable');
 });
